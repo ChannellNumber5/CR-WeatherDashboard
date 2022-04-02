77079030dd89baaf6406cf760d9cd6ec9f659e98
Christina R <xsnachannell@gmail.com>
2022-04-01 22:18:30 -0700

    updates script.js with new API urls and outline for information needed to pull

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var country = "USA";
 
 function getWeatherApi() {
 
-    var fetchUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + stateCode + "," + country + "&appid=" + ApiKey;
+    var fetchUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + stateCode + "," + country + "&appid=" + ApiKey;
 
     fetch(fetchUrl)
         .then(function (response) {
@@ -13,10 +13,13 @@ function getWeatherApi() {
         })
         .then(function (data) {
             console.log(data);
-            console.log(data.coord.lat);
-            console.log(data.coord.lon);
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
+            console.log(data[0].lat);
+            console.log(data[0].lon);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+    
+            //object keys needed: city, weather icon, date, temp, windspeed, humidity, UV-index with color corresponding block/button
             getForecastApi(lat,lon);
             // for(let i = 0; i < data.length; i++) {
 
@@ -25,14 +28,19 @@ function getWeatherApi() {
 }
 
 function getForecastApi(latInput, lonInput) {
-    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+ latInput + "&lon=" + lonInput + "&appid=" + ApiKey;
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ latInput + "&lon=" + lonInput + "&appid=" + ApiKey;
 
     fetch(forecastUrl)
-        .then(function(response) {
-            return response.json;
+        .then(function (response) {
+            return response.json();
         })
-        .then(function (forecast) {
-            console.log(forecast);
+        .then(function (forecastData) {
+            console.log(forecastData);
+            // let temp = data.main.temp;
+            // let windSpeed = data.weather.wind.speed;
+            // let humidity = data.main.humidity;
+            // let icon = data.main.icon;
+            //object keys needed: date, weather icon, temp, windspeed, humidity
         });
 }
 
