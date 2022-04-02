7bb354dc64f52b21b964d9e98aa67ca655803578
Christina R <xsnachannell@gmail.com>
2022-04-02 01:17:05 -0700

    updates README.md with resource links and updates script.js to pull correct data from OpenWeather API and creates variables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Resources
 - (Await and Fetch)[https://dmitripavlutin.com/javascript-fetch-async-await/]
+
+- (Converting Unix to Date)[https://www.delftstack.com/howto/javascript/javascript-convert-timestamp-to-date/]
+
+-(How to get OpenWeather Icon)[https://openweathermap.org/weather-conditions]
--- script.js
+++ script.js
@@ -36,6 +36,41 @@ function getForecastApi(latInput, lonInput) {
         })
         .then(function (forecastData) {
             console.log(forecastData);
+            var card = document.createElement("div");
+            card.setAttribute("class", "card");
+            card.setAttribute("style", "width: 18rem");
+            var iconImgCode = data.weather[0].icon;
+            var iconImg = document.createElement("img");
+            iconImg.setAttribute("class", "card-img-top");
+            iconImg.setAttribute("src", "http://openweathermap.org/img/wn/"+ iconImgCode + "@2x.png";
+            card.appendChild(iconImg);
+            var cardBody = createElement("div");
+            cardBody.setAttribute("class", "card-body");
+            var headerEl = document.createElement("h5");
+            headerEl.setAttribute("class", "card-title");
+            var timeStamp = forecastData.current.dt;
+            var date = new Date(timeStamp);
+            headerEl.textContent = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+            cardBody.appendChild(headerEl);
+            var paraEl = document.createElement("p");
+            paraEl.textContent = forecastData[0].description;
+            cardBody.appendChild(paraEl);
+            var listContainer = document.createElement("ul");
+            listContainer.setAttribute("class", "list-group list-group-flush");
+            var currentTemp = forecastData.current.temp - 273;
+            var currentTempEl = document.createElement("li");
+            currentTempEl.textContent = currentTemp + "&deg;" + "F";
+            var windSpeed = forecastData.current.wind_speed + "MPH";
+            var windSpeedEl = document.createElement("li");
+            var humidity = forecastData.current.humidity + "%";
+            var humidityEl = document.createElement("li");
+            var UvIndex = forecastData.current.uvi;
+            var UvIndexEl = document.createElement("li");
+            listContainer.appendChild(windSpeedEl);
+            listContainer.appendChild(humidityEl);
+            listContainer.appendChild(UvIndexEl);
+            card.appendChild(listContainer);
+
             // let temp = data.main.temp;
             // let windSpeed = data.weather.wind.speed;
             // let humidity = data.main.humidity;
